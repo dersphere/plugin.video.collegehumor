9129d8a795ffd3585c72e3ba6960d4555791312f
sphere <sphere@dersphere.de>
2011-09-25 13:15:33 +0200

    cosmetics

--- addon.py
+++ addon.py
@@ -9,8 +9,7 @@ def show_categories():
     categories = scraper.getCategories()
     items = [{'label': category['title'],
               'url': plugin.url_for('show_videos', category=category['link'],
-                                    page='1')
-             } for category in categories]
+                                    page='1')} for category in categories]
     return plugin.add_items(items)
 
 
